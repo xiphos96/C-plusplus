8fba9617874c443f3b82a48e8ff765b4a03a34fd
xiphos96 <deathknt@donga.ac.kr>
2025-04-22 19:58:13 +0900

    2024/04/22

---
+++ [01] 윈도우즈 암호 프로그래밍/Source/[01] GMPAdd/GMPAdd/resource.h
@@ -0,0 +1,32 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// GMPAdd.rc에서 사용되고 있습니다.
+//
+#define ID_STATIC_NUMBER2               3
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_GMPADD_DIALOG               102
+#define IDR_MAINFRAME                   128
+#define ID_STATIC_NUMBER1               1000
+#define ID_EDTC_NUMBER1                 1001
+#define ID_EDTC_NUMBER2                 1002
+#define IDC_BTN_ADD                     1003
+#define IDC_BTN_MULTI                   1004
+#define IDC_BTN_ADDMULTI                1005
+#define IDC_EDT_ADD_RESULT              1006
+#define ID_STATIC_ADD_RESULT            1007
+#define IDC_EDIT3                       1008
+#define IDC_EDT_MUL_RESULT              1008
+#define ID_STATIC_MUL_RESULT            1009
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
