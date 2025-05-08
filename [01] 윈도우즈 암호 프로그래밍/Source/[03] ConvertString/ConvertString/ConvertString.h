9438a12c1c8ce3e53a792f2c519b5da7a603b85d
xiphos96 <deathknt@donga.ac.kr>
2025-05-08 16:42:16 +0900

    2025/05/08

---
+++ [01] 윈도우즈 암호 프로그래밍/Source/[03] ConvertString/ConvertString/ConvertString.h
@@ -0,0 +1,32 @@
+﻿
+// ConvertString.h: PROJECT_NAME 애플리케이션에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CConvertStringApp:
+// 이 클래스의 구현에 대해서는 ConvertString.cpp을(를) 참조하세요.
+//
+
+class CConvertStringApp : public CWinApp
+{
+public:
+	CConvertStringApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CConvertStringApp theApp;
---
+++ [01] 윈도우즈 암호 프로그래밍/Source/[03] ConvertString/ConvertString/ConvertStringDlg.h
@@ -0,0 +1,36 @@
+﻿
+// ConvertStringDlg.h: 헤더 파일
+//
+
+#pragma once
+
+#include "../Files/Include/gmp.h"
+#include "../Files/Include/crylib/byte_string.h"
+
+// CConvertStringDlg 대화 상자
+class CConvertStringDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CConvertStringDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CONVERTSTRING_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
